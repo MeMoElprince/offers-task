2ad628e60263b030f50545fa9c11606f4801ee55
Mustafa <memomeme621@gmail.com>
2025-07-22 20:16:27 +0300

    remove dist folder

--- dist/databases/mongodb/schema/offer.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const schema = new mongoose_1.Schema({
-    test: { type: String, required: true },
-}, {
-    timestamps: true,
-});
-exports.default = (0, mongoose_1.model)('Offer', schema);
